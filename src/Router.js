cf542af89db9c515dd9babdfc877f71de597e459
oliveiramaicon407-ux <oliveiramaicon407@gmail.com>
2026-08-31 19:26:25 -0300

    Componenets

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './Home.vue'
 import SaasTemplate from './components/SaasTemplate.vue'
+import Login from './components/Login.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/saas',
     name: 'SaasTemplate',
     component: SaasTemplate
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
